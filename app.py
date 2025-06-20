28f1369c50c683ce12a46d36c1ecc41aa7e7478c
markmcfc <166084246+MarkMCFC@users.noreply.github.com>
2025-06-20 02:52:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -614,8 +614,8 @@ def json_to_m3u(data, host_url):
                 
                 m3u_content += (
                     f'#EXTINF:-1 tvg-id="{channel_name}" group-title="{group_title}",{channel_name}\n'
-                    '#EXTVLCOPT:http-referrer=https://lefttoplay.xyz/\n'
-                    '#EXTVLCOPT:http-origin=https://lefttoplay.xyz\n'
+                    '#EXTVLCOPT:http-referrer=https://forcedtoplay.xyz/\n'
+                    '#EXTVLCOPT:http-origin=https://forcedtoplay.xyz\n'
                     '#EXTVLCOPT:http-user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1\n'
                     f'{proxy_url}\n\n'
                 )
